01244409c68e823a5c30574ebdcaa0e917218df5
masudbappy <masud.bappy90@gmail.com>
2018-10-08 21:15:30 +0600

    HomeController has been updated

--- src/main/java/com/masudbappy/springboot/controllers/HomeController.java
+++ src/main/java/com/masudbappy/springboot/controllers/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-
+//@ResponseBody it just return data not html page.
 public class HomeController {
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String index() {
         return "home";
